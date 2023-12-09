2a65c702adfa6b7c14d578a8a83426c9b77414c7
baken.wws <baken.wws@gmail.com>
2023-12-09 08:53:43 +0500

    add player

--- src/components/AppController.vue
+++ src/components/AppController.vue
@@ -1,6 +1,6 @@
 <script setup>
 import useVideo from '@/stores/video.store'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import PauseIcon from './icons/PauseIcon.vue'
 import PlayIcon from './icons/PlayIcon.vue'
 import VolumeHigh from './icons/VolumeHigh.vue'
@@ -18,6 +18,15 @@ const videoStore = useVideo()
 onMounted(() => {
   videoStore.location = videoStore.locations[0]
 })
+
+const vid = ref()
+
+function playVideo() {
+  vid.value.playVideo()
+}
+function pauseVideo() {
+  vid.value.pauseVideo()
+}
 </script>
 <template>
   <Transition name="controller-drawer">
@@ -26,7 +35,7 @@ onMounted(() => {
       class="bg-neutral-950/70 px-8 py-6 fixed max-w-lg w-full right-0 bottom-0 z-50"
     >
       <div class="flex flex-col gap-4">
-        <div class="flex flex-row gap-4">
+        <div class="flex flex-row gap-4 text-white">
           <button
             @click="videoStore.isPlay = !videoStore.isPlay"
             class="text-white bg-neutral-100 w-8 h-8 rounded-full flex justify-center items-center"
@@ -40,6 +49,42 @@ onMounted(() => {
           >
             <VolumeOff class="fill-neutral-950" v-if="videoStore.muted" />
             <VolumeHigh class="fill-neutral-950" v-else />
+          </button>
+          <button
+            @click="playVideo"
+            class="text-white bg-neutral-100 w-8 h-8 rounded-full flex justify-center items-center"
+          >
+            <svg
+              class="fill-neutral-950"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+            >
+              <path
+                fill="current"
+                d="M400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-422h240v160H560v400q0 66-47 113t-113 47Z"
+              />
+            </svg>
+          </button>
+          <button
+            @click="pauseVideo"
+            class="text-white bg-neutral-100 w-8 h-8 rounded-full flex justify-center items-center"
+          >
+            <svg
+              class="fill-neutral-950"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+            >
+              <path
+                fill="current"
+                d="M792-56 56-792l56-56 736 736-56 56ZM560-514l-80-80v-246h240v160H560v166ZM400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-62l80 80v120q0 66-47 113t-113 47Z"
+              />
+            </svg>
           </button>
         </div>
         <div>
@@ -82,6 +127,11 @@ onMounted(() => {
       </div>
     </div>
   </Transition>
+  <YouTube
+    class="hidden"
+    src="https://www.youtube.com/embed/jfKfPfyJRdk?si=MavHzkfecOp8emho"
+    ref="vid"
+  ></YouTube>
 </template>
 <style>
 .controller-drawer-enter-active,
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import YouTube from 'vue3-youtube'
 
 const app = createApp(App)
 
@@ -11,5 +12,6 @@ const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+app.component('YouTube', YouTube)
 
 app.mount('#app')
